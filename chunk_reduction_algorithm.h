c3928f104d3d9c92c63d1284859bdc9d6c2823fa
JingzeChen <jingzechennm@gmail.com>
2018-10-30 11:07:35 +0800

    add chunk_reduction_algorithm.h

---
+++ chunk_reduction_algorithm.h
@@ -0,0 +1,20 @@
+#include<cstdio>
+#include<iostream>
+#ifndef PHAT_CUDA_COMMON_H
+#define PHAT_CUDA_COMMON_H
+
+#include <cstdint>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
+
+#include "cuda_runtime_api.h"
+#include "cuda_runtime.h"
+#include "cuda.h"
+
+#include "mallocMC.hpp"
+
+#include "gpu_boundary_matrix.h"
+
+
+
